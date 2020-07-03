897201df5ea80a1128d74b553b29c4359d5c1832
Stuil <1634829010@qq.com>
2020-07-03 18:19:51 +0800

    add

---
+++ prentBoot/proc/src/main/java/com/stuil/proc/server/IndexServer.java
@@ -0,0 +1,11 @@
+package com.stuil.proc.server;
+
+/**
+ * @description: 纯测试提交包
+ * @date: 2020/7/3
+ * @author: zwh
+ * @version: 1.0
+ */
+
+public interface IndexServer {
+}
---
+++ prentBoot/proc/src/main/java/com/stuil/proc/utils/IndexUtil.java
@@ -0,0 +1,13 @@
+package com.stuil.proc.utils;
+
+/**
+ * @title: IndexUtil
+ * @description: 纯测试提交包
+ * @date: 2020/7/3
+ * @author: zwh
+ * @copyright: Copyright (c) 2020
+ * @version: 1.0
+ */
+
+public class IndexUtil {
+}
