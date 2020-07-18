79845d88b167a4d5003fba70338bbadc9576daba
Stuil <35212735+Stuil@users.noreply.github.com>
2020-07-19 00:41:31 +0800

    config--mybatis

---
+++ prentBoot/src/main/java/com/stuil/proc/service/ISysUserService.java
@@ -0,0 +1,16 @@
+package com.stuil.proc.service;
+
+import com.stuil.proc.entity.SysUser;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author stuil
+ * @since 2020-07-18
+ */
+public interface ISysUserService extends IService<SysUser> {
+
+}
