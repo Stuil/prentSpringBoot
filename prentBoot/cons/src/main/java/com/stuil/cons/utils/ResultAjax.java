67e722cfd10a1aacde1c853391765296d6467875
Stuil <1634829010@qq.com>
2020-08-06 18:02:24 +0800

    login

---
+++ prentBoot/cons/src/main/java/com/stuil/cons/utils/ResultAjax.java
@@ -0,0 +1,32 @@
+package com.stuil.cons.utils;
+
+import lombok.Data;
+
+/**
+ * @title: ResultAjax
+ * @description: 公共返回
+ * @author: stuil
+ * @copyright: Copyright (c) 2020
+ * @version: 1.0
+ */
+@Data
+public class ResultAjax {
+    int code;
+    String msg;
+    Object object;
+
+   public static ResultAjax success(Object object){
+        ResultAjax resultAjax=new ResultAjax();
+        resultAjax.setCode(0);
+        resultAjax.setMsg("成功");
+        resultAjax.setObject(object);
+        return resultAjax;
+    }
+
+    public static   ResultAjax fail(int code,String msg){
+        ResultAjax resultAjax=new ResultAjax();
+        resultAjax.setCode(code);
+        resultAjax.setMsg(msg);
+        return resultAjax;
+    }
+}
