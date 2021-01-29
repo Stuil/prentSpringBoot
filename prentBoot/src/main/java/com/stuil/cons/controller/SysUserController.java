c2ddc903cb1cb52b5563efb2aae8ff0efd94d5a6
zwh <zwh1634829010@gmail.com>
2021-01-29 17:37:42 +0800

    add
    add 新增邮件发送功能

--- prentBoot/src/main/java/com/stuil/cons/controller/SysUserController.java
+++ prentBoot/src/main/java/com/stuil/cons/controller/SysUserController.java
@@ -1,10 +1,8 @@
 package com.stuil.cons.controller;
 
 
-import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.core.toolkit.EncryptUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.stuil.cons.entity.SysUser;
 import com.stuil.cons.mapper.SysUserMapper;
@@ -12,20 +10,16 @@ import com.stuil.cons.service.SysUserService;
 import com.stuil.cons.utils.EncUtil;
 import com.stuil.cons.utils.LayuiResp;
 import com.stuil.cons.utils.ResultAjax;
-import org.nutz.ioc.loader.annotation.Inject;
+import com.stuil.cons.utils.email.EmailUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.sql.Wrapper;
-import java.util.List;
 
 /**
  * <p>
@@ -102,7 +96,7 @@ public class SysUserController {
     @ResponseBody
     public LayuiResp page(Integer page,Integer limit){
         IPage<SysUser> userIPage = new Page<>();
-        userIPage = mapper.selectPage(new Page<>(page, limit), null);
+        userIPage = mapper.selectPage(new Page<SysUser>(page, limit), null);
         return LayuiResp.createBySuccess(userIPage.getTotal(),userIPage.getRecords());
     }
 
